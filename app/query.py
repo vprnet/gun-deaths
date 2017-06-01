6ab7d5440e48adec9d489fd25a7031b649539e78
SARA SIMON <smbsimon@gmail.com>
2017-06-01 17:07:02 -0400

    start building out queries to sort through the database

--- app/query.py
+++ app/query.py
@@ -2,5 +2,40 @@
 from sheet import get_google_sheet
 
 def get_entries():
-    full_list = get_google_sheet()
-    return full_list
+    sheet = get_google_sheet()
+    entry_list = []
+
+    for entry in sheet:
+        entry['Name'] = entry['Legal Name']
+        entry['Sex'] = str.lower(entry['Sex'])
+        entry['Date'] = entry['Date of Death']
+        entry['Manner'] = str.lower(entry['Manner of Death'])
+
+        if entry['If described, type of firearm']:
+            entry['Specified'] = str.lower(entry['If described, type of firearm'])
+        else:
+            entry['Specified'] = "firearm"
+
+        entry_list.append(entry)
+
+    return entry_list
+
+
+# PARSE FOR--
+# legal name
+# date of death
+# time of death
+# age
+# marital status
+# education
+# us army?
+# occupation
+# race
+# relationship of informant to decedent
+# manner of death
+# type of firearm
+
+
+# STILL NEED--
+# a way to deal with dates of death where only a month is given?
+# a better way to deal with entries where the firearm isn't specified
